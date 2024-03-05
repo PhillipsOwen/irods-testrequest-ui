ab7ac9d107fe20d5e4d98a251fc394a1acc6ee6f
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-03-05 13:48:09 -0500

    readme and irods logo updates

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,8 +1,6 @@
-// SPDX-FileCopyrightText: 2024 Renaissance Computing Institute. All rights reserved.
+// Copyright (c) 2024, The University of North Carolina at Chapel Hill All rights reserved.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-License-Identifier: LicenseRef-RENCI
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BSD 3-Clause License
 
 export default function PageFooter(props){
     // builds the footer on the page
