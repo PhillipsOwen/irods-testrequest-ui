92840297fc851386278f9700d147403c20450bcf
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-03-21 14:23:18 -0400

    tidying up layout

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -5,30 +5,29 @@
 import {Container, Navbar, Row} from "reactstrap";
 import {Link} from "react-router-dom";
 
-export default function PageFooter(){
+export default function PageFooter() {
     // builds the footer on the page
     return (
         <div>
-        <footer className="footer">
-            <Container>
-                <Row>
-                    <div>
-                        <Navbar>
-                            <nav>
-                                <div style={{color: "white", marginTop: "5px"}}>
-                                    <Link style={{color: "white"}} to="/About">About</Link>
+            <footer className="footer">
+                <Container>
+                    <Row>
+                        <div>
+                            <Navbar>
+                                <nav>
+                                    <div style={{color: "white", marginTop: "5px"}}>
+                                        <Link style={{color: "white"}} to="/About">About</Link>
 
-                                    <span style={{marginLeft: "30px"}}>
-                                        Version: {process.env.REACT_APP_VERSION}
-                                    </span>
-                                </div>
-                            </nav>
-                        </Navbar>
-                    </div>
-                </Row>
-            </Container>
-        </footer>
+                                        <span style={{marginLeft: "30px"}}>
+                                            Version: {process.env.REACT_APP_VERSION}
+                                        </span>
+                                    </div>
+                                </nav>
+                            </Navbar>
+                        </div>
+                    </Row>
+                </Container>
+            </footer>
         </div>
-    )
-        ;
+    );
 }
