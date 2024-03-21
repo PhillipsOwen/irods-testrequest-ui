10d3d1c2f214cef02700ee45ed9a5676bb760922
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-03-21 13:27:54 -0400

    tidying up the layout, moving the version to the footer

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -7,38 +7,30 @@ import {Link} from "react-router-dom";
 
     export default function PageHeader() {
         return (
-            <>
-                <Navbar style={{backgroundColor: '#18bc9c'}} expand="xs" className="border-bottom border-gray">
+            <div style={{backgroundColor: "0a534e"}}>
+                <Navbar expand="xs" className="border-top border-gray">
                     <Col className="d-flex justify-content-start">
-                        <NavbarBrand href="/About">
+                        <NavbarBrand>
                             <img alt="logo" src={process.env.PUBLIC_URL + '/iRODS-Logo-RTM_white.png'} style={{height: 40}}/>
                         </NavbarBrand>
-                    </Col>
 
-                    <Col className="d-flex justify-content-center">
                         <NavbarBrand className="d-inline-block p-0">
                             <h1 style={{color: "white"}}>Test request</h1>
                         </NavbarBrand>
                     </Col>
-
-                    <Col className="d-flex justify-content-end">
-                        <span style={{color: "white"}}>Version: {process.env.REACT_APP_VERSION}</span>
-                    </Col>
                 </Navbar>
-                <Navbar expand="xs" className="border-bottom border-gray">
+                <Navbar expand="xs" className="border-top border-bottom border-gray">
                     <Row>
                         <Col>
-                        <div>
                             <div>
                                 <nav>
-                                    <Link style={{color: "White", align: "left", marginRight: "100px"}} to="/">Request a new test run</Link>
+                                    <Link style={{color: "White", align: "left", marginLeft: "50px", marginRight: "100px"}} to="/">Request a new test run</Link>
                                     <Link style={{color: "White", align: "left"}} to="/WatchStatus">Follow the run progress</Link>
                                 </nav>
                             </div>
-                        </div>
                         </Col>
                     </Row>
                 </Navbar>
-            </>
+            </div>
         );
     }
